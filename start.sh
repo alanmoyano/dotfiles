7a2a287cece6cc7ccdd2ecb020478498077c4a8b
Alan (Linux) <alanmoyano203@gmail.com>
2024-02-11 03:33:42 -0300

    Cuarta versión del script de configuración inicial

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ sudo nala install zsh tldr btop gh unzip exa fzf ripgrep curl neovim -y
 # Oh My Posh
 mkdir $HOME/.oh-my-posh
 curl -s https://ohmyposh.dev/install.sh | bash -s -- -d $HOME/.oh-my-posh/
-curl -s https://raw.githubusercontent.com/alanmoyano/dotfiles/main/alan.omp.json > $HOME/.oh-my-posh/
+curl -s https://raw.githubusercontent.com/alanmoyano/dotfiles/main/alan.omp.json > $HOME/.oh-my-posh/alan.omp.json
 
 # Exportando el .zshrc
 echo '# Oh My Posh
