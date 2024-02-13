ebd1dd9e56aaab4b0fdabd54cd84e742b3b2d05a
Alan Moyano <alanmoyano203@gmail.com>
2024-02-13 16:34:05 -0300

    Hay que usar Oh My Zsh

--- start.sh
+++ start.sh
@@ -6,22 +6,34 @@ sudo apt install nala -y
 sudo nala upgrade -y
 sudo nala install zsh tldr btop gh unzip exa fzf ripgrep curl neovim bat keychain -y
 
-# Oh My Posh
-mkdir $HOME/.oh-my-posh
-curl -s https://ohmyposh.dev/install.sh | bash -s -- -d $HOME/.oh-my-posh/
-curl -s https://raw.githubusercontent.com/alanmoyano/dotfiles/main/alan.omp.json > $HOME/.oh-my-posh/alan.omp.json
+# Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Exportando el .zshrc
-echo '# Oh My Posh
-export PATH="$HOME/.oh-my-posh:$PATH"
-eval "$(oh-my-posh init zsh --config ~/.oh-my-posh/alan.omp.json)"
+echo 'if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+export ZSH="$HOME/.oh-my-zsh"
+
+ZSH_THEME="powerlevel10k/powerlevel10k"
+
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
 
 # Alias
 alias cls=clear
 alias ls="exa --icons"
 alias ll="ls -l"
 alias l="ls -lah"
-alias lt="ll -TL=2"' > ~/.zshrc
+alias lt="ll -TL=2"
+alias bat="batcat --paging=never"
+
+source $ZSH/oh-my-zsh.sh
+
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >~/.zshrc
 
 # Poniendo zsh como shell
 sudo chsh -s /bin/zsh $(whoami)
