002fcd2c7b84e43186d24c3d0c17730f9c773e17
Alan Moyano <alanmoyano203@gmail.com>
2024-02-13 16:59:37 -0300

    Update Oh My Zsh installation to be unattended

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ sudo nala upgrade -y
 sudo nala install zsh tldr btop gh unzip exa fzf ripgrep curl neovim bat keychain -y
 
 # Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # Powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
