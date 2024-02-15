06e9bc91bf224a3c09d48e0f9b9631c51b1817c5
Alan Moyano <alanmoyano203@gmail.com>
2024-02-14 21:24:09 -0300

    Instalando los plugins

--- start.sh
+++ start.sh
@@ -9,6 +9,10 @@ sudo nala install zsh tldr btop gh unzip exa fzf ripgrep curl neovim bat keychai
 # Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
+# Plugins de Zsh
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 # Powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
