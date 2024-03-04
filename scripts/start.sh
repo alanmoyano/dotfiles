1cbfc907491963b7a1740cacb0ed5b74617fdf06
Alan (Linux) <alanmoyano203@gmail.com>
2024-03-04 01:27:28 -0300

    Añadido fd a la lista de cosas a instalar

--- scripts/start.sh
+++ scripts/start.sh
@@ -25,7 +25,7 @@ source .bashrc
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Instalando todo
-brew install gcc gh fzf wget curl git tlrc neovim ripgrep unzip btop zsh bat starship eza file
+brew install gcc gh fzf wget curl git tlrc neovim ripgrep unzip btop zsh bat starship eza file fd
 
 # Activando los bindings de fzf
 /home/linuxbrew/.linuxbrew/opt/fzf/install --all
