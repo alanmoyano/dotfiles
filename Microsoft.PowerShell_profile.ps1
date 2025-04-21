b657bbac5609cdc565a67428485af3d7dc112f97
Alan Moyano <alanmoyano203@gmail.com>
2025-04-21 00:14:01 -0300

    Actualizando

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,36 +1,75 @@
+# Set the console’s encoding to UTF‑8 for input and output
+[Console]::OutputEncoding = [System.Text.UTF8Encoding]::new()
+[Console]::InputEncoding  = [System.Text.UTF8Encoding]::new()
+
+# Set the LANG environment variable if some tools rely on it
+$env:LANG = "es-ES.UTF-8"
+
 # Prompt
 oh-my-posh init pwsh --config "$HOME\.config\dotfiles\alan.omp.json" | Invoke-Expression
 
-# Icons
-Import-Module Terminal-Icons
-
-# Cargar Alias
-Import-Module -Name HackF5.ProfileAlias -Force -Global -ErrorAction SilentlyContinue
-
 # PSFzf
+$env:FZF_DEFAULT_COMMAND = "fd --type file --strip-cwd-prefix"
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 $commandOverride = [ScriptBlock]{ param($Location) Write-Host $Location }
 Set-PsFzfOption -AltCCommand $commandOverride
 
 # Alias Básicos
 Set-Alias touch New-Item
-Set-ProfileAlias rf "rm -Recurse -Force #{*}" -Bash -Force > nul
+Set-Alias unzip Expand-Archive
+Set-Alias open Invoke-Item
+
+function rf {
+	rm -Recurse -Force $args
+}
 
 # Alias del ls
-Set-ProfileAlias ls "eza --icons=always #{*}" -Bash -Force > nul
-Set-ProfileAlias ll "eza --icons=always -lh --git #{*}" -Bash -Force > nul
-Set-ProfileAlias lt "eza --icons=always -lhTL=2 --git #{*}" -Bash -Force > nul
-Set-ProfileAlias l "eza --icons=always -lah --git #{*}" -Bash -Force > nul
+Remove-Alias ls -ErrorAction SilentlyContinue
+function ls {
+  eza --icons=always --group-directories-first $args
+}
+
+function ll {
+  eza --icons=always --group-directories-first -lh --git $args
+}
+
+function lt{
+	eza --icons=always --group-directories-first -lhTL=2 --git $args
+}
+
+function l{
+	eza --icons=always --group-directories-first -lah --git $args
+}
 
 # Alias de Git
 Set-Alias gg git
-Set-ProfileAlias gga 'git add #{*}' -Bash -Force > nul
-Set-ProfileAlias ggc 'git commit -m #{*}' -Bash -Force > nul
-Set-ProfileAlias ggd 'git diff #{*}' -Bash -Force > nul
-Set-ProfileAlias ggf 'git fetch #{*}' -Bash -Force > nul
-Set-ProfileAlias ggpll 'git pull #{*}' -Bash -Force > nul
-Set-ProfileAlias ggpsh 'git push #{*}' -Bash -Force > nul
-Set-ProfileAlias ggs 'git status #{*}' -Bash -Force > nul
+function gga{
+	git add $args
+}
+
+function ggc{
+	git commit -m $args
+}
+
+function ggd{
+	git diff $args
+}
+
+function ggf{
+	git fetch $args
+}
+
+function ggpll{
+	git pull $args
+}
+
+function ggpsh{
+	git push $args
+}
+
+function ggs{
+	git status $args
+}
 
 # Utilities
 function which($command){
@@ -46,4 +85,9 @@ function .lnx(){
 	Set-Location \\wsl.localhost\Ubuntu\home\alan
 }
 
+$env:YAZI_FILE_ONE="C:\Program Files\Git\usr\bin\file.exe"
+
 fnm env --use-on-cd | Out-String | Invoke-Expression
+fnm completions | Out-String | Invoke-Expression
+uv generate-shell-completion powershell | Out-String | Invoke-Expression
+zoxide init powershell | Out-String | Invoke-Expression
